5c995df849532c60f9fcdc501a28bf4c6b435168
pheiduck <pheiduck@users.noreply.github.com>
2020-11-28 15:11:52 +0100

    Regular auto-commit at 15:11:52 on 28/11/2020

--- commit.sh
+++ commit.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-cd $(pwd)
+for x in {0..100} ; do
+    if (( x % 10 == 0 ))
+    then
+        cd $(pwd)
 git add --all
 timestamp() {
   date +"at %H:%M:%S on %d/%m/%Y"
@@ -9,3 +12,6 @@ timestamp() {
 git commit -am "Regular auto-commit $(timestamp)"
 
 ping -c5 www.github.com && git push origin --all || echo "not connected"
+    fi
+    sleep 1
+done
