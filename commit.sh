d92369e8fa0ece3fcc05d59bdd6eb0dddcab01e9
168912916 859421311 <pheiduck@github.com>
2021-02-21 13:12:51 +0100

    Regular auto-commit at 13:12:51 on 21/02/2021

--- commit.sh
+++ commit.sh
@@ -23,7 +23,7 @@ timestamp() {
   date +"at %H:%M:%S on %d/%m/%Y"
 }
 
-git commit -am "Regular auto-commit $(timestamp)"
+git commit -sam "Regular auto-commit $(timestamp)"
 
 ping -c5 www.github.com && git push origin --all || echo "not connected"
     sleep 1
